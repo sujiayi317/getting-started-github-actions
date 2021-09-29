9c47beed019448833cce85d47867c954c6b97472
Jiayi Su <47668906+sujiayi317@users.noreply.github.com>
2021-09-29 12:28:53 -0400

    Create github-actions-script.sh

---
+++ github-actions-script.sh
@@ -0,0 +1,19 @@
+echo What is in this directory?
+ls -a
+echo
+echo Is Java installed?
+java -version
+echo
+echo Is Git installed?
+git --version
+echo What about build tools?
+mvn --version
+gradle --version
+ant -version
+echo Who is running this script?
+whoami
+echo How is the disk laid out?
+df
+echo What environment variables are available?
+env
+echo
